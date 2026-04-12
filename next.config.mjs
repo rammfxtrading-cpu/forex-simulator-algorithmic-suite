2010595724c4e838a0ce82854637a64bb84d4a1e
Ramon Mesa <rammfxtrading@gmail.com>
2026-04-12 12:24:11 +0200

    fix: exclude lightweight-charts from SSR bundle

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Prevent lightweight-charts from being bundled on the server
+      config.externals = [...(config.externals || []), 'lightweight-charts']
+    }
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
