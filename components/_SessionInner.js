1dd910fa98cc7155ddcb5f307ce2514199bbd5c7
Ramon Mesa <rammfxtrading@gmail.com>
2026-04-16 12:23:27 +0200

    fix: rightOffset back to 50

--- components/_SessionInner.js
+++ components/_SessionInner.js
@@ -552,7 +552,7 @@ export default function SessionPage(){
       if(typeof window!=='undefined') window.__algSuiteSeriesData=[...agg,...cr.phantom]
       if(prev===0&&!cr.hasLoaded){
         cr.chart.timeScale().scrollToPosition(8,false)
-        try{cr.chart.timeScale().applyOptions({barSpacing:12,rightOffset:300})}catch{}
+        try{cr.chart.timeScale().applyOptions({barSpacing:12,rightOffset:50})}catch{}
         cr.hasLoaded=true
       }
     } else {
